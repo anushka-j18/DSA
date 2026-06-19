4d31d94777ad201a788c747e288ab811bb752b20
anushka-j18 <anushka.kp.joshi@gmail.com>
2026-06-19 22:18:43 +0530

    Add files via upload

---
+++ pattern10.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class pattern10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
