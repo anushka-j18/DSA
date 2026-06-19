3fa3abbcee7c109677f8ba582de8d2ce802461bb
anushka-j18 <anushka.kp.joshi@gmail.com>
2026-06-19 20:15:00 +0530

    Add files via upload

---
+++ pattern7.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            // Print spaces
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
