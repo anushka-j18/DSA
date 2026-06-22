e98c0e5cb1dbb7760e758b40ccb441b4c94a2ec7
anushka-j18 <anushka.kp.joshi@gmail.com>
2026-06-22 18:40:34 +0530

    Add files via upload

---
+++ PalindromeString.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+
+    static boolean isPalindrome(String s, int left, int right) {
+        if (left >= right) {
+            return true;
+        }
+
+        if (s.charAt(left) != s.charAt(right)) {
+            return false;
+        }
+
+        return isPalindrome(s, left + 1, right - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+
+        System.out.println(isPalindrome(s, 0, s.length() - 1));
+
+        sc.close();
+    }
+}
