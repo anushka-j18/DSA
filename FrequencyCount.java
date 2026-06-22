b122db10315a51da010864e12f8bb3e33c2f5c39
anushka-j18 <anushka.kp.joshi@gmail.com>
2026-06-22 18:45:46 +0530

    Add files via upload

---
+++ FrequencyCount.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println("[" + entry.getKey() + ", " + entry.getValue() + "]");
+        }
+
+        sc.close();
+    }
+}
