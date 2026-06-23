63fbcf6d8f434f2d5aced0af83f4a5383fd7691e
anushka-j18 <anushka.kp.joshi@gmail.com>
2026-06-23 12:44:46 +0530

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MergeSort {
+
+    static void mergeSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+
+        int mid = left + (right - left) / 2;
+
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+
+        merge(arr, left, mid, right);
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+            if (arr[i] <= arr[j]) {
+                temp[k++] = arr[i++];
+            } else {
+                temp[k++] = arr[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
+        }
+
+        while (j <= right) {
+            temp[k++] = arr[j++];
+        }
+
+        for (int p = 0; p < temp.length; p++) {
+            arr[left + p] = temp[p];
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        mergeSort(nums, 0, n - 1);
+
+        System.out.println(Arrays.toString(nums));
+
+        sc.close();
+    }
+}
