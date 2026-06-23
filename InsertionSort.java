72e01e05ca00494cb5498da43bd03833d650a921
anushka-j18 <anushka.kp.joshi@gmail.com>
2026-06-23 12:42:29 +0530

    Add files via upload

---
+++ InsertionSort.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        // Insertion Sort
+        for (int i = 1; i < n; i++) {
+            int key = nums[i];
+            int j = i - 1;
+
+            while (j >= 0 && nums[j] > key) {
+                nums[j + 1] = nums[j];
+                j--;
+            }
+
+            nums[j + 1] = key;
+        }
+
+        System.out.println(Arrays.toString(nums));
+
+        sc.close();
+    }
+}
