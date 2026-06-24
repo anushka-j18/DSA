87b019d93ea4e6bc99d072855660f172cea32458
anushka-j18 <anushka.kp.joshi@gmail.com>
2026-06-24 21:29:57 +0530

    Add files via upload

---
+++ SecondLargest.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class SecondLargest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+
+        System.out.println(
+            secondLargest == Integer.MIN_VALUE ? -1 : secondLargest
+        );
+
+        sc.close();
+    }
+}
