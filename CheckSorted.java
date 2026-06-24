55cb17ba20f00b3d9d1e9968b73ee1d5ecaf603c
anushka-j18 <anushka.kp.joshi@gmail.com>
2026-06-24 21:35:33 +0530

    Add files via upload

---
+++ CheckSorted.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class CheckSorted {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        boolean isSorted = true;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+
+        System.out.println(isSorted);
+
+        sc.close();
+    }
+}
